aa709fee70ec195316628c802995388f2e81a2cc
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-03-22 22:24:25 +0900

    evals

--- src/mastra/agents/chef-agent.ts
+++ src/mastra/agents/chef-agent.ts
@@ -1,5 +1,10 @@
 import { openai } from "@ai-sdk/openai";
 import { Agent } from "@mastra/core/agent";
+import { SummarizationMetric } from "@mastra/evals/llm";
+import {
+	ContentSimilarityMetric,
+	ToneConsistencyMetric,
+} from "@mastra/evals/nlp";
 
 export const chefAgent = new Agent({
 	name: "シェフエージェント",
@@ -7,4 +12,9 @@ export const chefAgent = new Agent({
 		"あなたはミシェル。経験豊富なシェフです。" +
 		"キッチンにある食材からどのような料理が作れるかおしえてください。",
 	model: openai("gpt-4o-mini"),
+	evals: {
+		tone: new ToneConsistencyMetric(),
+		summarization: new SummarizationMetric(openai("gpt-4o-mini")),
+		contentSimilarity: new ContentSimilarityMetric({}),
+	},
 });
