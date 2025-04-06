b4f9830db032d76c13d6f41d29afe78a278cb401
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-04-07 07:48:43 +0900

    vercel setting

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -6,12 +6,19 @@ import { stockAgent } from "./agents/stock-agent";
 import { candidateWorkflow } from "./workflows/workflow";
 import { prReviewAgent } from "./agents/pr-reqview-agent";
 import { prReviewWorkflow } from "./workflows";
+import { VercelDeployer } from "@mastra/deployer-vercel";
 
 export const mastra = new Mastra({
 	workflows: { candidateWorkflow, prReviewWorkflow },
 	agents: { weatherAgent, chefAgent, stockAgent, prReviewAgent },
 	logger: createLogger({
-		name: "Mastra",
+		name: "try-mastra",
 		level: "info",
+	}),
+	deployer: new VercelDeployer({
+		teamSlug: "harusame-dev",
+		projectName: "try-mastra",
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		token: process.env.VERCEL_TOKEN!,
 	}),
 });
